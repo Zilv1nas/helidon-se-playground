19727b3551ef812c270e4a0c37c9a8b5ba48b30e
Žilvinas Bliudžius <zilvinas.bliudzius@inventi.lt>
2023-02-28 17:01:43 +0200

    remove unecessary print statement

--- src/main/kotlin/com/github/zilv1nas/service/EventConsumer.kt
+++ src/main/kotlin/com/github/zilv1nas/service/EventConsumer.kt
@@ -16,29 +16,25 @@ abstract class EventConsumer<E : Event>(
     val channel: Channel<EventEnvelope<E>> = Channel.create(channelName)
 
     val listener: SubscriberBuilder<Message<EventEnvelope<E>>, Void> =
-        ReactiveStreams.builder<Message<EventEnvelope<E>>>()
-            .onError {
-                println(it)
-            }
-            .forEach { message ->
-                val event = message.payload.data
-                logger.info("Consuming event: $event")
+        ReactiveStreams.builder<Message<EventEnvelope<E>>>().forEach { message ->
+            val event = message.payload.data
+            logger.info("Consuming event: $event")
 
-                val spanContext = extractTracing(message.payload.metadata)
-                val tracingCallbacks = spanContext?.let {
-                    trace(channel.name(), it)
-                }
-
-                try {
-                    on(event)
-                    message.ack()
-                    tracingCallbacks?.let { it.onSuccess() }
-                } catch (e: Throwable) {
-                    logger.error("Could not consume event $event", e)
-                    tracingCallbacks?.let { it.onFailure(e) }
-                    throw e
-                }
+            val spanContext = extractTracing(message.payload.metadata)
+            val tracingCallbacks = spanContext?.let {
+                trace(channel.name(), it)
             }
+
+            try {
+                on(event)
+                message.ack()
+                tracingCallbacks?.let { it.onSuccess() }
+            } catch (e: Throwable) {
+                logger.error("Could not consume event $event", e)
+                tracingCallbacks?.let { it.onFailure(e) }
+                throw e
+            }
+        }
 
     protected abstract fun on(event: E)
 }
